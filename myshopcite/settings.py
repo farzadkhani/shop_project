48631f995f336c6638a8e557421793d0358d549f
farzadkhani <fkhanigm@gmail.com>
2021-05-17 13:13:14 +0430

    modifier for heroku

--- myshopcite/settings.py
+++ myshopcite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -197,3 +198,6 @@ SIMPLE_JWT = {
     # 'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     # 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
