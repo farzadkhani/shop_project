cd078a9515ac5926afbe68103fbf37648e5fe414
farzadkhani <fkhanigm@gmail.com>
2021-05-18 12:06:33 +0430

    make allow host and debug fix

--- myshopcite/settings.py
+++ myshopcite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wb%!4517pdre1b(ys&$08q524dt8s3yg@0o0y!f)8=p=3&9iwy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'myshopway.herokuapp.com']
 AUTH_USER_MODEL ='Accounts.User'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
